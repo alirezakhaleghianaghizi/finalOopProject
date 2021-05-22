13a94f90a9b97f638b5e00f114b5d3923c16324d
vahid-ansari <vahid.ansari00108@gmail.com>
2021-05-22 11:49:03 +0430

    controll animals developed

--- src/controller/ControllerAnimal.java
+++ src/controller/ControllerAnimal.java
@@ -1,5 +1,25 @@
 package controller;
 
+import model.animal.asisstant.Cat;
+import model.animal.defender.Dog;
+import model.animal.producer.Bufallo;
+import model.animal.producer.Chicken;
+import model.animal.producer.Turkey;
+import model.animal.wild.Bear;
+import model.animal.wild.Lion;
+import model.animal.wild.Tiger;
+
+import java.util.ArrayList;
+
 public class ControllerAnimal {
     //Array list of all animal
+    ArrayList<Tiger> tigers = new ArrayList<>();
+    ArrayList<Bear> bears = new ArrayList<>();
+    ArrayList<Lion> lions = new ArrayList<>()
+    ArrayList<Dog> dogs = new ArrayList<>();
+    ArrayList<Cat> cats = new ArrayList<>();
+    ArrayList<Chicken> chickens = new ArrayList<>();
+    ArrayList<Bufallo> bufallos= new ArrayList<>();
+    ArrayList<Turkey> turkeys  = new ArrayList<>();
+
 }
