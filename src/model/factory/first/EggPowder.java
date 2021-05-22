8b3f15d75055816756162189ab111b172a2006dd
alirezakhaleghianaghizi <alirezakhaleghianaghizi@gmail.com>
2021-05-22 10:46:56 +0430

    checkingbranchchage

--- src/model/factory/first/EggPowder.java
+++ src/model/factory/first/EggPowder.java
@@ -4,6 +4,7 @@ import model.factory.Factory;
 
 public class EggPowder extends Factory {
 
+    //adddadadadadda
     public EggPowder(String name, double x, double y) {
         super("EggPowder", x, y,4,150);
     }
